f5ba7043e041a35baaccd806ff7538cfc4da61dc
Patrik Eriksson <74836180+Tiramisioux@users.noreply.github.com>
2026-09-22 22:28:43 +0200

    driver: add experimental 12-bit UHD crop

--- imx283.c
+++ imx283.c
@@ -752,7 +752,7 @@ static const struct imx283_mode supported_modes_12bit[] = {
 		.crop = CENTERED_RECTANGLE(imx283_active_area, 5016, 3648),
 	},
 	{
-		/* Mode 0, 12-bit 1x1, 1.78:1 crop -- 25.9 MB/frame */
+		/* Mode 0, 12-bit 1x1, 16:9 crop -- 25.9 MB/frame */
 		.mode = IMX283_MODE_0,
 		.bpp = 12,
 		.width = 5472 + 96,
@@ -794,7 +794,7 @@ static const struct imx283_mode supported_modes_12bit[] = {
 		.crop = CENTERED_RECTANGLE(imx283_active_area, 5472, 2956),
 	},
 	{
-		/* Mode 0, 12-bit 1x1, 1.89:1 crop -- 24.3 MB/frame */
+		/* Mode 0, 12-bit 1x1, 17:9 crop -- 24.3 MB/frame */
 		.mode = IMX283_MODE_0,
 		.bpp = 12,
 		.width = 5472 + 96,
@@ -981,6 +981,28 @@ static const struct imx283_mode supported_modes_12bit[] = {
 		.horizontal_ob = 96,
 		.vertical_ob = 16,
 		.crop = CENTERED_RECTANGLE(imx283_active_area, 5472, 2144),
+	},
+	{
+		/* Mode 0, 12-bit 1x1, 16:9 UHD crop -- 12.8 MB/frame */
+		.mode = IMX283_MODE_0,
+		.bpp = 12,
+		.width = 3840 + 96,
+		.height = 2160 + 16,
+		.min_HMAX = 887,
+		.min_VMAX = 3793,
+		.crop_min_VMAX = 2305,          /* 2176 + 129 */
+		.default_HMAX = 900,
+		.default_VMAX = 4000,
+		.min_SHR = 12,
+		.veff = 3694,
+		.vst = 0,
+		.vct = 0,
+		.hbin_ratio = 1,
+		.vbin_ratio = 1,
+		.horizontal_ob = 96,
+		.vertical_ob = 16,
+		.crop = CENTERED_RECTANGLE(imx283_active_area, 3840, 2160),
+		.experimental = true,
 	},
 };
 static const struct imx283_mode supported_modes_10bit[] = {
